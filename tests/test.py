a3dc413ef251396efe57a4f1a087a3e3465190df
Eli <eli.serra173@gmail.com>
2024-11-28 13:52:31 +0100

    avoid errors on DATA_DIR value

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,24 @@
 """test : open feed and plot image."""
 
 import datetime
+import os
 import time
-
 import matplotlib
 import matplotlib.pyplot as plt
 from multigraph import check_starting_nan
 from PyFina import getMeta, PyFina
 
 FEED_NB = 1
-DATA_DIR = "./datas"
+DATA_DIR = os.path.join(
+    os.path.abspath(os.getcwd()),
+    "datas"
+)
+if not os.path.isdir(DATA_DIR):
+    DATA_DIR = os.path.join(
+        os.path.abspath(os.getcwd()),
+        "tests",
+        "datas"
+    )
 meta = getMeta(FEED_NB, DATA_DIR)
 print(meta)
 STEP = 3600
