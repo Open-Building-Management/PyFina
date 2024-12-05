26bb446da75d9774261b345624eaa9c1aa84982b
Alexandre CUER <alexandre.cuer@wanadoo.fr>
2024-12-05 16:46:04 +0100

    Integrate mano8 datas in tests/test

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 from multigraph import check_starting_nan
 from PyFina import getMeta, PyFina
 
-FEED_NB = 1
 DATA_DIR = os.path.join(
     os.path.abspath(os.getcwd()),
     "datas"
@@ -22,28 +21,54 @@ if not os.path.isdir(DATA_DIR):
     if not os.path.isdir(DATA_DIR):
         raise FileNotFoundError(f"Could not find data directory in any location: {DATA_DIR}")
 
-meta = getMeta(FEED_NB, DATA_DIR)
-print(meta)
 STEP = 3600
-start = meta["start_time"]
-length = meta["npoints"] * meta["interval"]
-WINDOW = min(8 * 24 * 3600, length)
 
-nbpts = WINDOW // STEP
-temp_ext = PyFina(FEED_NB, DATA_DIR, start, STEP, nbpts)
+def generate_episode(feed_nb: int, feed_name: str, feed_unit: str, **kwargs):
+    """visualize initial episode."""
+    meta = getMeta(feed_nb, DATA_DIR)
+    print(meta)
+    # start
+    start = kwargs.get("start", meta["start_time"])
 
-check_starting_nan("température extérieure", temp_ext)
+    # length to display in seconds
+    length = meta["npoints"] * meta["interval"]
+    if not kwargs.get("view_whole", False):
+        length = min(length, 8 * 24 * 3600)
+    length = kwargs.get("seconds", length)
+    print(length)
 
-localstart = datetime.datetime.fromtimestamp(start)
-utcstart = datetime.datetime.fromtimestamp(start, datetime.timezone.utc)
-title = f"starting on :\nUTC {utcstart}\n{time.tzname[0]} {localstart}"
-figure = plt.figure(figsize = (10, 10))
-matplotlib.rc('font', size=8)
-plt.subplot(111)
-plt.title(title)
-plt.ylabel("outdoor Temp °C")
-plt.xlabel("time in hours")
-plt.plot(temp_ext)
-figure.savefig(f"feed_{FEED_NB}.png")
-if os.environ.get('DISPLAY_PLOTS', '').lower() == 'true':
-    plt.show()
+    # step
+    use_original_step = kwargs.get("use_original_step", False)
+    step = meta["interval"] if use_original_step else STEP
+
+    datas = PyFina(feed_nb, DATA_DIR, start, step, length // step)
+    check_starting_nan(feed_name, datas)
+    localstart = datetime.datetime.fromtimestamp(start)
+    utcstart = datetime.datetime.fromtimestamp(start, datetime.timezone.utc)
+    title = f"starting on :\nUTC {utcstart}\n{time.tzname[0]} {localstart}"
+    figure = plt.figure(figsize = (10, 10))
+    matplotlib.rc('font', size=8)
+    plt.subplot(111)
+    plt.title(title)
+    plt.ylabel(f"{feed_name} - {feed_unit}")
+    plt.xlabel("time in hours")
+    plt.plot(datas)
+    figure.savefig(f"nb_{feed_nb}_{feed_name}.png")
+    if os.environ.get('DISPLAY_PLOTS', '').lower() == 'true':
+        plt.show()
+
+if __name__ == "__main__":
+    generate_episode(1, "température extérieure", "°C")
+    generate_episode(
+        2,
+        "mano8 recordings whole",
+        "°C",
+        view_whole=True
+    )
+    generate_episode(
+        2,
+        "mano8 recordings",
+        "°C",
+        start=1540166400 - 3600 * 24,
+        seconds=24 * 3600
+    )
